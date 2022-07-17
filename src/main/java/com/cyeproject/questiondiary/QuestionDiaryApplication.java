f53f7494eaf2dee7e093538e196a888ca9a5b365
Choi-Young-Eun <cye8891@naver.com>
2022-07-18 00:13:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/cyeproject/questiondiary/QuestionDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.cyeproject.questiondiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionDiaryApplication.class, args);
+	}
+
+}
